ae7729c0b748ac55c034152e86077200f7956776
khuevotan <76434350+khuevotan@users.noreply.github.com>
2021-11-02 21:45:01 +0700

    frist commit

---
+++ STM32_TenKhue/STM32_TenKhue/MDK-ARM/RTE/_STM32_TenKhue/RTE_Components.h
@@ -0,0 +1,15 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'STM32_TenKhue' 
+ * Target:  'STM32_TenKhue' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+
+#endif /* RTE_COMPONENTS_H */
