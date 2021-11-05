d702f7dc6ccb0dac8b5f8ace5d743129c0f1320b
khuevotan <76434350+khuevotan@users.noreply.github.com>
2021-11-05 22:10:37 +0700

    Cap nhat

---
+++ NC_NgatDonGianDen_STM32/Ngat4den/MDK-ARM/RTE/_Ngat4den/RTE_Components.h
@@ -0,0 +1,15 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Ngat4den' 
+ * Target:  'Ngat4den' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+
+#endif /* RTE_COMPONENTS_H */
